3786f65acb3fbe9e035a895b5e52bcef6f3514eb
cadubcm <caducbs@outlook.com>
2026-04-15 20:33:33 -0300

    Entrega 2.2

---
+++ Entrega 3/crud-orm/src/db/schema.ts
@@ -0,0 +1,23 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const uf = sqliteTable("uf", {
+  id:    text("id").primaryKey(),
+  nome:  text("nome").notNull(),
+  sigla: text("sigla").notNull().unique(),
+});
+
+export const cidade = sqliteTable("cidade", {
+  id:    text("id").primaryKey(),
+  nome:  text("nome").notNull(),
+  uf_id: text("uf_id")
+    .notNull()
+    .references(() => uf.id),
+});
+
+export const regiao = sqliteTable("regiao", {
+  id:        text("id").primaryKey(),
+  nome:      text("nome").notNull(),
+  cidade_id: text("cidade_id")
+    .notNull()
+    .references(() => cidade.id),
+});
