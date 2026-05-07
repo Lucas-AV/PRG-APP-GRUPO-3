0efc516c3ff2ecf778ae99d754eeb8b4de985555
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2026-05-07 16:17:14 -0300

    espaçamento no conteudo do botão de texto

--- conecta-app/components/ui/gradient-button.tsx
+++ conecta-app/components/ui/gradient-button.tsx
@@ -45,6 +45,7 @@ const styles = StyleSheet.create({
   },
   gradient: {
     paddingVertical: Spacing.base,
+    paddingHorizontal: Spacing.xxl,
     alignItems: 'center',
     justifyContent: 'center',
   },
